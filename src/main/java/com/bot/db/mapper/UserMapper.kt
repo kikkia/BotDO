64fb89f73cb7c461139f33235474f3b38c94a326
JessWalters <jessw@iastate.edu>
2021-02-08 18:56:04 -0600

    fixes

--- src/main/java/com/bot/db/mapper/UserMapper.kt
+++ src/main/java/com/bot/db/mapper/UserMapper.kt
@@ -6,11 +6,11 @@ import net.dv8tion.jda.api.entities.Member
 class UserMapper {
     companion object {
         fun map(member: Member) : User {
-            return User(member.user.id, member.user.name, member.user.name)
+            return User(member.user.id, member.user.name)
         }
 
         fun map(user: net.dv8tion.jda.api.entities.User) : User {
-            return User(user.id, user.name, user.name)
+            return User(user.id, user.name)
         }
     }
 }
