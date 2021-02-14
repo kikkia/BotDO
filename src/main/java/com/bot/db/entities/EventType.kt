968e9c4a683ae3e9da8c02642a6cff9d98d256cb
JessWalters <jessw@iastate.edu>
2021-02-14 01:37:30 -0600

    simplify event types

---
+++ src/main/java/com/bot/db/entities/EventType.kt
@@ -0,0 +1,4 @@
+package com.bot.db.entities
+
+class EventType {
+}
--- src/main/resources/db/migration/V1.2__add_events.sql
+++ src/main/resources/db/migration/V1.2__add_events.sql
@@ -1,9 +1,3 @@
-CREATE TABLE IF NOT EXISTS `event_type` (
-    `id` INT(32) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `description` VARCHAR(2047) NOT NULL)
-ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS `event` (
     `id` INT(32) NOT NULL AUTO_INCREMENT,
     `author` VARCHAR(255) NOT NULL,
@@ -16,8 +10,5 @@ CREATE TABLE IF NOT EXISTS `event` (
         REFERENCES `guild` (`id`),
     CONSTRAINT `event_author_fk`
         FOREIGN KEY (`author`)
-        REFERENCES `users` (`id`),
-    CONSTRAINT `event_type_event`
-        FOREIGN KEY (`event_type`)
-        REFERENCES `event_type` (`id`))
+        REFERENCES `users` (`id`))
 ENGINE = InnoDB;
