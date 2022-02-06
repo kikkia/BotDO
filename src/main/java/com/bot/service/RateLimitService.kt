5d5eea0cf78e54704ad118b13403cec9df7b88cb
kikkia <jesswalters53@gmail.com>
2022-02-06 02:01:18 -0600

    add ratelimiting to lookups

---
+++ src/main/java/com/bot/service/RateLimitService.kt
@@ -0,0 +1,28 @@
+package com.bot.service
+
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket
+import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import io.github.bucket4j.Bucket4j
+import io.github.bucket4j.Refill
+import java.time.Duration
+
+
+@Service
+class RateLimitService {
+    private val rateLimitMap: MutableMap<String, Bucket>
+    init {
+        rateLimitMap = ConcurrentHashMap()
+    }
+
+    fun resolveBucket(userId: String): Bucket {
+        return rateLimitMap.computeIfAbsent(userId) { id: String -> newBucket(id) }
+    }
+
+    private fun newBucket(apiKey: String): Bucket {
+        return Bucket4j.builder()
+                .addLimit(Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1))))
+                .build()
+    }
+}
