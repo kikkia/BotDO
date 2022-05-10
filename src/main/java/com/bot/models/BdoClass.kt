5ec92682491cdc4badcd5b6db611673921eaa370
kikkia <sabledawn@gmail.com>
2022-05-10 00:11:00 -0700

    lowercase drak

--- src/main/java/com/bot/models/BdoClass.kt
+++ src/main/java/com/bot/models/BdoClass.kt
@@ -27,7 +27,7 @@ enum class BdoClass(val display: String, val aliases: List<String>, val stateful
     NOVA("Nova", emptyList(), true),
     SAGE("Sage", listOf("sadge"), true),
     CORSAIR("Corsair", listOf("mermaid"), true),
-    DRAK("Drakania", listOf("Drak"), false);
+    DRAK("Drakania", listOf("drak"), false);
 
     companion object {
         fun getClassFromName(name: String) : BdoClass? {
